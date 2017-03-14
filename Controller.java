562e62ec977735898ec679b27c2c4f8453c9198b
Vecochan <vedrana.zeba@gmail.com>
2017-03-14 23:14:53 +0100

    Revert "Lagt till endel"
    This reverts commit f774fb3d3b126c292a4e778c3f1b248d29f03310.

--- Controller.java
+++ Controller.java
@@ -1,39 +1,56 @@
 package clientSystem;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.io.IOException;
 import java.net.UnknownHostException;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 
 public class Controller {
 
 
 	public static void main(String []args) throws UnknownHostException, IOException, InterruptedException{
 		int port =  2533; //Ändra till valfri port vid behov
-		String ip = "192.168.1.103";//"192.168.1.2"; //Ändra till er ip fe80::8cd6:cf6:5828:32c%13
+		String ip = "192.168.1.103"; //Ändra till er ip
+		Server server = new Server(port);
 
-		Server1 server = new Server1(port);
-		Client client1 = new Client("Sandra", ip, port);
+		Client client1 = new Client("Vedrana", ip, port);
+		Client client2 = new Client("Adda", ip, port);
+		Client client3 = new Client("Sandra", ip, port);
+		Client client4 = new Client("Evelyn", ip, port);
 
-		Client client2 = new Client("Vedrana", ip, port);
-		client2.connect();
-		
-		Thread.sleep(2000);
-		System.out.println("Sending now...");
-
-		client2.sendMessage("Sandra", "Hej Sandra");
-		client2.sendMessage("Sandra", "Fick du detta? Eller ej?");
-		client2.sendIcon("Sandra", " ");
-		Thread.sleep(2000);
 		client1.connect();
-		Thread.sleep(2000);
+		client2.connect();
+		client3.connect();
+		client4.connect();
+
+		Thread.sleep(1000);
+		client1.sendMessage("Bara");
+		Thread.sleep(1000);
+		client1.sendMessage("Så");
+		Thread.sleep(1000);
+		client1.sendMessage("Ni");
+		Thread.sleep(1000);
+		client1.sendMessage("Vet");
+		client2.sendMessage("Så funkar det!");
+		client2.disconnect();
+		Thread.sleep(1000);
+		client2.connect();
+		Thread.sleep(1000);
+		client2.sendMessage("JAG ÄR TILLBAKA!!!");
+		Thread.sleep(1000);
+
+		client2.disconnect();
+		client1.disconnect();
+
+		Thread.sleep(1000);
+
+
+		client3.sendMessage("Tja! Någon här?");
+		Thread.sleep(500);
+		client4.sendMessage("Jag är här!?");
+
+		server.disconnect();
+		
+		client1.connect();
+		
 
 	}
 
